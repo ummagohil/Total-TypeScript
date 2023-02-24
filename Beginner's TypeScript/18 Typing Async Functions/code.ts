73b4f095863f1e9aa3ccfab5b65881545a487981
ummagohil <umgo0510@gmail.com>
2023-02-24 12:05:42 +0000

    lesson 18

---
+++ Beginner's TypeScript/18 Typing Async Functions/code.ts
@@ -0,0 +1,37 @@
+import { expect, it } from "vitest";
+
+interface User {
+	id: string;
+	firstName: string;
+	lastName: string;
+}
+
+type GetUser = (id: string) => Promise<User>; // for example if you needed data from a db like prisma
+
+const createThenGetUser = async (
+	createUser: () => Promise<string>,
+	getUser: GetUser,
+): Promise<User> => {
+	const userId: string = await createUser();
+
+	const user = await getUser(userId);
+
+	return user;
+};
+
+it("Should create the user, then get them", async () => {
+	const user = await createThenGetUser(
+		async () => "123",
+		async (id) => ({
+			id,
+			firstName: "Matt",
+			lastName: "Pocock",
+		}),
+	);
+
+	expect(user).toEqual({
+		id: "123",
+		firstName: "Matt",
+		lastName: "Pocock",
+	});
+});
